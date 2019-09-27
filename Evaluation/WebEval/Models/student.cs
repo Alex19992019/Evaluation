2a1b8572057f7038cc512c814a8206d079761a54
Alex Salazar <alexxsala@gmail.com>
2019-09-27 18:11:11 -0400

    commit

---
+++ Evaluation/WebEval/Models/student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebEval.Models
+{
+    public class student
+    {
+        [Key]
+        [Required]
+        public int StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public enum gender
+        {
+            masculino,
+            femenino
+        }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int Califications { get; set; }
+
+        public enum studentStatus
+        {
+            Aprobado,
+            Reprobado
+
+        }
+        [Required]
+
+        public studentStatus Status { get; set; }
+
+        public gender Gender { get; set; }
+    }
+}
---
+++ Evaluation/WebEval/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebEval.Startup))]
+namespace WebEval
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
